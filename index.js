3ee2e502a8802550691c4022ce1f12ab3a966688
Lucy Sanders <lucyhsanders@gmail.com>
2019-10-27 17:15:50 -0600

    de es6-ified

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 function handleSubmitAmounts(event) {
   event.preventDefault();
-  const allowAmount = new Amount(["allowed", "billed", "listed"]);
-    displayHtml("allow", `$${allowAmount.allow}`);
+  const allowAmount = Amount(["allowed", "billed", "listed"]);
+    displayHtml("allow", "$" + allowAmount.allow);
     displayHtml("name", allowAmount.name)
 }
-class Amount {
-  constructor(namesArr) {
-    this.data = this.setData(namesArr)
-    const { allowed, billed, listed } = this.data;
-    this.allow = this.setAllowAmount(allowed, billed, listed);
-    this.name = this.setName(this.data, this.allow);
-  }
-  setData(namesArr) {
-    const values = namesArr.map(name => {
-      return parseInt($(`#${name}`).val())
-    })
-    const data = {};
-    values.forEach((value, index) => {
-      data[namesArr[index]] = value;
-    })
-    return data;
-  }
-  setAllowAmount(allowed, billed, listed) {
-    return allowed > listed && billed > listed 
-    ? allowed > billed
-    ? billed
-    : allowed
-    : allowed > billed
-      ? allowed
-      : billed;
-  }
-  setName(data, allow) {
-    return Object.keys(data).reduce((acc, key) => {
-     if(data[key] === allow) {
-       acc = key;
-     }
-     return acc;
-   }, 0)
-   }
+function Amount(namesArr) {
+  this.data = this.setData(namesArr)
+  const allowed = this.data.allowed;
+  const billed = this.data.billed;
+  const listed = this.data.listed;
+  this.allow = this.setAllowAmount(allowed, billed, listed);
+  this.name = this.setName(this.data, this.allow);
 }
-
+Amount.prototype.setData = function(namesArr) {
+  const values = namesArr.map(function(name) {
+    return parseInt($("#" + name).val())
+  })
+  const data = {};
+  values.forEach(function(value, index) {
+    data[namesArr[index]] = value;
+  })
+  return data;
+}
+Amount.prototype.setAllowAmount = function(allowed, billed, listed) {
+  return allowed > listed && billed > listed 
+  ? allowed > billed
+  ? billed
+  : allowed
+  : allowed > billed
+    ? allowed
+    : billed;
+}
+Amount.prototype.setName = function(data, allow) {
+  return Object.keys(data).reduce(function(acc, key) {
+    if(data[key] === allow) {
+      acc = key;
+    }
+    return acc;
+  }, 0)
+}
 function displayHtml(name, value) {
-  $(`#${name}`).html(value)
+  $("#" + name).html(value)
 }
